68c07b9a5228de36dfe9d7cb7ebee5c37be7384a
sheilahd <sheila.hdez@gmail.com>
2020-07-17 21:30:35 -0400

    changing mistakes q4

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 //Is time to create things in the DOM!!. We want you to create a new 'div' Element and append it as a child of the element that has the 'id = a-1' (the element is in index.html file)
 // all right our new element needs style. For that, assign to the new element the className 'square'!. Each time the user clicks on the new element it has to respond changing the displayed text 'x' to 'o'
 
-
-
-
 //resolve // QUESTION 1 here
-
 
 const divOrig = document.getElementById('a-1');
 const divElem = document.createElement('div');
@@ -31,11 +27,6 @@ divElem.addEventListener('click', () => {
 //you also need to create a button that makes the remove action of the element selected
 // append the new list as a child of the element with the 'id = a-2'
 // append the button as a child of the element with the 'id = a-2'
-
-
-
-
-
 //resolve // QUESTION 2 here
 
 const colors = ['red', 'white', 'black', 'green', 'orange'];
@@ -61,9 +52,6 @@ removeItemButton.addEventListener('click', () => {
 
 });
 
-
-
-
 //------------------------Question 3 ---------------------------
 //Now lets mix some maths and programming. In the function below (calculate_sphere) write a JavaScript program to calculate the volume and surface area of a sphere.
 //take the radius from the input value element with the 'id=radius' in the index.html file
@@ -85,9 +73,6 @@ const calculate_sphere = () => {
 };
 
 window.onload = document.getElementById('MyForm').onsubmit = calculate_sphere; // this execute the volume_sphere function each time the calculate (submit) button is clicked
-
-
-
 
 
 //------------------------Question 4 ---------------------------
@@ -129,16 +114,15 @@ let questionThree = questions[2];
 
 buttonOne.addEventListener('click', () => {
 
-   
-        if (questionOne.style.visibility === "hidden") {
-            questionOne.style.visibility = "visible";
-            buttonOne.innerText = "Hide question1";
+    if (questionOne.style.visibility === "hidden") {
+        questionOne.style.visibility = "visible";
+        buttonOne.innerText = "Hide question1";
 
-        } else {
-            questionOne.style.visibility = "hidden";
-            (buttonOne.innerText = "Show question1");
+    } else {
+        questionOne.style.visibility = "hidden";
+        (buttonOne.innerText = "Show question1");
 
-        }
+    }
 });
 
 buttonTwo.addEventListener('click', () => {
